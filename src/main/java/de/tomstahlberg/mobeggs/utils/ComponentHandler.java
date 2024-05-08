11d608968a6bf038e70c1ed0512d0ed9e6a1db2f
Tom Stahlberg <tom.stahlberg@jakkit.de>
2024-05-08 16:56:34 +0200

    Added setLore api as ComponentHandler

--- src/main/java/de/tomstahlberg/mobeggs/utils/ComponentHandler.java
+++ src/main/java/de/tomstahlberg/mobeggs/utils/ComponentHandler.java
@@ -4,11 +4,25 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 
 public class ComponentHandler {
     public static void setDisplayName(ItemStack itemStack, String message){
         ItemMeta itemMeta = itemStack.getItemMeta();
         Component component = Component.text(message);
         itemMeta.displayName(component);
+        itemStack.setItemMeta(itemMeta);
+    }
+    public static void setLore(ItemStack itemStack, List<String> loreList){
+        List<Component> components = new ArrayList<>();
+        for(String loreLine : loreList){
+            components.add(Component.text(loreLine));
+        }
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.lore(components);
+        itemStack.setItemMeta(itemMeta);
     }
 }
